30ee5ea82f3e8989d632b0fcba9ddb3d6b34bae5
maomao7777 <lp77dog@hotmail.com.tw>
2024-10-15 03:12:34 +0800

    cust init method and minor typo fix

--- board/raspberrypi4-64/post-build.sh
+++ board/raspberrypi4-64/post-build.sh
@@ -6,7 +6,10 @@ set -e
 rm -rf ${TARGET_DIR}/storage
 mkdir ${TARGET_DIR}/storage
 install ${BASEDIR}/fwtool.conf  ${TARGET_DIR}/etc/
-install ${BASEDIR}/initOverlay.sh  ${TARGET_DIR}/etc/init.d/S00initOverlay.sh
+install ${BASEDIR}/fwupd.sh  ${TARGET_DIR}/etc/
+install ${BASEDIR}/initOverlay.sh  ${TARGET_DIR}/etc/init.d/S00initOverlay
+install ${BASEDIR}/initNetconf.sh  ${TARGET_DIR}/etc/init.d/S40network
+
 # Add a console on tty1
 if [ -e ${TARGET_DIR}/etc/inittab ]; then
     grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
@@ -14,6 +17,12 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
 if [ -e ${TARGET_DIR}/etc/inittab ]; then
-    sed -i -e '/^[^#].*\/bin\/umount/s/^/#/' ${TARGET_DIR}/etc/inittab
-    echo "::shutdown:/bin/umount -a -f -r" >> ${TARGET_DIR}/etc/inittab
+    if grep -q '^[^#]*::shutdown:/bin/umount -a -r' ${TARGET_DIR}/etc/inittab && 
+        ! grep -q '^[^#]*::shutdown:/bin/umount -a -r -f' ${TARGET_DIR}/etc/inittab; then
+    sed -i 's|::shutdown:/bin/umount -a -r|::shutdown:/bin/umount -a -r -f|' ${TARGET_DIR}/etc/inittab
+    echo "instead '::shutdown:/bin/umount -a -r' to '::shutdown:/bin/umount -a -r -f' in ${TARGET_DIR}/etc/inittab"
+    fi
 fi
+
+current_time=$(date +"%Y-%m-%d %H:%M:%S")
+echo "build time: $current_time" > ${TARGET_DIR}/etc/rversion
