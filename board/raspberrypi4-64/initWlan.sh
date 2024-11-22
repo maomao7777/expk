bb0a618ada27b8fb772a4455c5ef2d961b7bc978
maomao7777 <lp77dog@hotmail.com.tw>
2024-11-22 13:27:43 +0800

    [rpi4] 1.add telnetd and remove inet busybox 2.update wlan script

--- board/raspberrypi4-64/initWlan.sh
+++ board/raspberrypi4-64/initWlan.sh
@@ -3,6 +3,7 @@
 BRIDGE_NAME="br0"
 WIFI_INTERFACE="wlan0"
 ETHERNET_INTERFACE="eth0"
+INIT_STATIC_IP="192.168.0.15"
 
 
 mkdir -p /run/network
@@ -12,16 +13,18 @@ case "$1" in
 	printf "init network config: "
 	modprobe brcmfmac
 	modprobe cfg80211
+	sleep 0.5
 	ifconfig $WIFI_INTERFACE down
 	ifconfig $ETHERNET_INTERFACE down
-	ifconfig $BRIDGE_NAME up
 	brctl addbr $BRIDGE_NAME
 	brctl addif $BRIDGE_NAME $WIFI_INTERFACE
 	brctl addif $BRIDGE_NAME $ETHERNET_INTERFACE
 	ifconfig $WIFI_INTERFACE up
 	ifconfig $ETHERNET_INTERFACE up
+	ifconfig $BRIDGE_NAME $INIT_STATIC_IP up
+	ifconfig lo up
 
-	udhcpc -i $BRIDGE_NAME
+	udhcpc -i $BRIDGE_NAME -b
 	hostapd -B /storage/hostapd.conf
 	;;
   stop)
