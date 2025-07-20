8215bedb2c4ca0bb3503ffef580e6fc07da0da68
maomao7777 <lp77dog@hotmail.com.tw>
2025-07-21 01:52:15 +0800

    [rpi4]fixing and clear wireless setup logic

--- expk/board/raspberrypi4-64/initNetconf.sh
+++ expk/board/raspberrypi4-64/initNetconf.sh
@@ -4,9 +4,9 @@ BRIDGE_NAME="br0"
 WIFI_INTERFACE="wlan0"
 ETHERNET_INTERFACE="eth0"
 INIT_STATIC_IP="192.168.0.15"
-INIT_WL_FILE="/storage/hostapdStor.conf"
 TARGET_WL_LINK="/etc/hostapdTest.conf"
-IS_WL_CONF="/storage/wpa_supplicant.conf"
+IS_WLAP_CONF=/storage/is_wlap.conf
+IS_WLCLT_CONF=/storage/is_wlclt.conf
 
 
 mkdir -p /run/network
@@ -19,29 +19,29 @@ case "$1" in
 	sleep 0.5
 	ifconfig $WIFI_INTERFACE down
 	ifconfig $ETHERNET_INTERFACE down
+	if [ -f "$IS_WLCLT_CONF" ]; then
+	echo "wireless client mode detected"
+	else
+	echo "wireless ap mode detected"
 	brctl addbr $BRIDGE_NAME
-	brctl addif $BRIDGE_NAME $WIFI_INTERFACE
 	brctl addif $BRIDGE_NAME $ETHERNET_INTERFACE
+	fi
 	ifconfig $WIFI_INTERFACE up
 	ifconfig $ETHERNET_INTERFACE up
 	ifconfig $BRIDGE_NAME $INIT_STATIC_IP up
 	ifconfig lo up
-	if [ -f "$IS_WL_CONF" ]; then
-	wpa_supplicant -B -i $WIFI_INTERFACE -c $IS_WL_CONF
+	if [ -f "$IS_WLCLT_CONF" ]; then
+	wpa_supplicant -B -i $WIFI_INTERFACE -c $IS_WLCLT_CONF
 	udhcpc -i $WIFI_INTERFACE -s /etc/exdhcpc.script -b
 	else
 	udhcpc -i $BRIDGE_NAME -s /etc/exdhcpc.script -b
-	if [ -f "$INIT_WL_FILE" ]; then
-		if [ -e "$TARGET_WL_LINK" ]; then
-			rm -f "$TARGET_WL_LINK"
-			echo "Removed existing $TARGET_WL_LINK."
-		fi
-		ln -s "$INIT_WL_FILE" "$TARGET_WL_LINK"
-		echo "Linked $INIT_WL_FILE to $TARGET_WL_LINK."
+	if [ -f "$IS_WLAP_CONF" ]; then
+	echo "setup ap through $IS_WLAP_CONF."
+	hostapd -B $IS_WLAP_CONF
 	else
-	echo "$INIT_WL_FILE does not exist using orig_df."
-	fi
+	echo "$IS_WLAP_CONF does not exist using orig_df."
 	hostapd -B $TARGET_WL_LINK
+	fi
 	fi
 	httpd -h /root/web -c /etc/httpd.conf -v
 	telnetd
--- expk/board/raspberrypi4-64/web/cgi-bin/set_wireless.sh
+++ expk/board/raspberrypi4-64/web/cgi-bin/set_wireless.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+IS_WLAP_CONF=/storage/is_wlap.conf
+IS_WLCLT_CONF=/storage/is_wlclt.conf
+
 echo "Content-type: text/html"
 echo ""
-
 read POST_DATA
 ACTION=$(echo "$POST_DATA" | grep -oE 'action=[^&]+' | cut -d= -f2)
 ACTION=$(echo "$ACTION" | sed 's/+/ /g')
@@ -11,13 +13,13 @@ PASS=$(echo "$decoded_data" | sed -n 's/.*password=\([^&]*\).*/\1/p')
 
 case "$ACTION" in
     "Update WL Client")
-        echo "update_config=1" > /storage/is_wlclt.conf
-        echo "country=TW" >> /storage/is_wlclt.conf
-        echo "network={" >> /storage/is_wlclt.conf
-        echo "   ssid=$SSID" >> /storage/is_wlclt.conf
-        echo "   key_mgmt=WPA-PSK" >> /storage/is_wlclt.conf
-        echo "   psk=$PASS" >> /storage/is_wlclt.conf
-        echo "}" >> /storage/is_wlclt.conf
+        echo "update_config=1" > $IS_WLCLT_CONF
+        echo "country=TW" >> $IS_WLCLT_CONF
+        echo "network={" >> $IS_WLCLT_CONF
+        echo "   ssid=\"$SSID\"" >> $IS_WLCLT_CONF
+        echo "   key_mgmt=WPA-PSK" >> $IS_WLCLT_CONF
+        echo "   psk=\"$PASS\"" >> $IS_WLCLT_CONF
+        echo "}" >> $IS_WLCLT_CONF
         echo "<html><body><h1>WL Client Updated</h1>"
         echo "<p>Conneting SSID : $SSID</p>"
         echo "<p>Conneting Password: $PASS</p>"
@@ -25,20 +27,20 @@ case "$ACTION" in
         echo "</body></html>"
         ;;
     "Update AP")
-        rm /storage/is_wlclt.conf
+        rm $IS_WLCLT_CONF
         echo "SSID=$SSID" > /etc/ap_config
         echo "PASS=$PASS" >> /etc/ap_config
-        echo "interface=wlan0" > /storage/hostapdStor.conf
-        echo "bridge=br0" >> /storage/hostapdStor.conf
-        echo "driver=nl80211" >> /storage/hostapdStor.conf
-        echo "ssid=$SSID" >> /storage/hostapdStor.conf
-        echo "hw_mode=g" >> /storage/hostapdStor.conf
-        echo "channel=11" >> /storage/hostapdStor.conf
-        echo "wpa=2" >> /storage/hostapdStor.conf
-        echo "wpa_passphrase=$PASS" >> /storage/hostapdStor.conf
-        echo "wpa_key_mgmt=WPA-PSK" >> /storage/hostapdStor.conf
-        echo "rsn_pairwise=CCMP" >> /storage/hostapdStor.conf
-        echo "ieee80211n=1" >> /storage/hostapdStor.conf
+        echo "interface=wlan0" > $IS_WLAP_CONF
+        echo "bridge=br0" >> $IS_WLAP_CONF
+        echo "driver=nl80211" >> $IS_WLAP_CONF
+        echo "ssid=$SSID" >> $IS_WLAP_CONF
+        echo "hw_mode=g" >> $IS_WLAP_CONF
+        echo "channel=11" >> $IS_WLAP_CONF
+        echo "wpa=2" >> $IS_WLAP_CONF
+        echo "wpa_passphrase=$PASS" >> $IS_WLAP_CONF
+        echo "wpa_key_mgmt=WPA-PSK" >> $IS_WLAP_CONF
+        echo "rsn_pairwise=CCMP" >> $IS_WLAP_CONF
+        echo "ieee80211n=1" >> $IS_WLAP_CONF
         echo "<html><body><h1>AP Updated</h1>"
         echo "<p>New SSID: $SSID</p>"
         echo "<p>New Password: $PASS</p>"
